c52ce10119ec7fab24716a90eb2834ae2092c585
rayyan <rayyan10@bellsouth.net>
2021-02-04 00:02:45 -0600

    📦  FEAT: [a4] continue spec implementation

--- A4/a4.py
+++ A4/a4.py
@@ -5,41 +5,78 @@ import sys
 import json
 
 CREATED = 0
+TOWNS = []
+PLACEMENT_COMMANDS = []
+INVALID_PLACEMENT = []
 
 
-def createTownNetwork(params):
-    return {}
+def responseFromServer():
+    #TODO: respond with boolean, also print invalid placements one by one
 
 
-def placeCharacter(params):
-    return {}
+def createTownNetwork(params: list):
+    for road in params:
+        if road["from"] not in TOWNS:
+            TOWNS.append(road["from"])
+        if road["to"] not in TOWNS:
+            TOWNS.append(road["to"])
+    msg = {"towns": TOWNS, "roads": params}
+    return msg
 
 
-def queryPassageSafety(params):
-    return {}
+def placeCharacter(params: dict):
+    if params['town'] not in TOWNS:
+        msg = ["invalid placement", {"name": params["character"], "town": params["town"]}]
+        #TODO: WHEN TO PRINT
+        INVALID_PLACEMENT.append(msg)
+    else:
+        placement = {"name": params["character"], "town": params["town"]}
+        PLACEMENT_COMMANDS.append(placement)
+        msg = {}
+    return msg
+
+
+def queryPassageSafety(params: dict):
+    characterInQuestion = params["character"]
+    destination = params["town"]
+    msg = {}
+    if destination not in TOWNS:
+        #TODO: error
+    else:
+        for placement in PLACEMENT_COMMANDS:
+            if placement["character"] == characterInQuestion:
+                # Batch query
+                query = {"character": characterInQuestion, "destination": destination}
+                msg = {"characters": PLACEMENT_COMMANDS, "query": query}
+                return json.dumps(msg)
+            else:
+                #TODO: error
+    return msg
 
 
 def parseUserCommand(commandDict: dict):
     # returns processed json Command
     global CREATED
-    msg = None
-    expectResponse = True
+    processedRequest = None
+    toReceive = False
+    toSend = True
     jsonKeys = commandDict.keys()
     wellformedCommand = "command" in jsonKeys and "params" in jsonKeys
     if wellformedCommand:
         if commandDict["command"] == "roads" and CREATED == 0:
-            msg = createTownNetwork(commandDict["params"])
+            processedRequest = createTownNetwork(commandDict["params"])
             CREATED += 1
-            expectResponse = False
         elif commandDict["command"] == "place":
-            msg = placeCharacter(commandDict["params"])
+            processedRequest = placeCharacter(commandDict["params"])
+            toSend = False
         elif commandDict["command"] == "passage-safe?":
-            msg = queryPassageSafety(commandDict["params"])
+            processedRequest = queryPassageSafety(commandDict["params"])
+            toReceive = True
     else:
-        msg = {"error": "not a request", "object": commandDict}
-        print(json.dumps(msg))
+        processedRequest = {"error": "not a request", "object": commandDict}
+        print(json.dumps(processedRequest))
 
-    return (json.dumps(msg), expectResponse)
+    return json.dumps(processedRequest), toSend, toReceive
 
 
 def main():
@@ -71,8 +108,23 @@ def main():
         sock.send(userName.encode("utf-8"))
 
         # Receive Session ID
-        # data = sock.recv(4096).decode("utf-8")
-        # print(data)
+        data = sock.recv(4096).decode("utf-8")
+        sessionId = json.dumps(['the server will call me', data])
+        print (sessionId)
+
+        # Get first command (should be roads)
+        userInput = input("Reading input > ")
+        jd = json.JSONDecoder()
+        commandDict = jd.decode(userInput)
+        if commandDict["command"] == "roads":
+            jsonRequest, toSend, toReceive = parseUserCommand(commandDict)
+            sock.send(jsonRequest.encode("utf-8"))
+            # TODO: can get rid of
+            CREATED += 1
+        else:
+            print("The first command must be a 'roads' command.")
+            exit(1)
+
     except:
         print("Connection failed during signup + session id exchange")
         exit(1)
@@ -83,9 +135,10 @@ def main():
             userInput = input("Reading input > ")
             jd = json.JSONDecoder()
             commandDict = jd.decode(userInput)
-            jsonRequest, expectServerResp = parseUserCommand(commandDict)
-            sock.send(jsonRequest.encode("utf-8"))
-            if expectServerResp:
+            jsonRequest, toSend, toReceive = parseUserCommand(commandDict)
+            if toSend:
+                sock.send(jsonRequest.encode("utf-8"))
+            if toReceive:
                 data = sock.recv(4096).decode("utf-8")
                 print(data)
         except json.JSONDecodeError:
